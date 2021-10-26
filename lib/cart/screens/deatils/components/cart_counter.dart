f83e56db45513cac108b0673f38e914eaa09ed29
Sahaj Sutariya <73480434+Sahaj90@users.noreply.github.com>
2021-10-26 14:51:09 +0530

    Update cart_counter.dart
    increment decrement

--- lib/cart/screens/deatils/components/cart_counter.dart
+++ lib/cart/screens/deatils/components/cart_counter.dart
@@ -3,10 +3,14 @@ import 'package:flutter/material.dart';
 import '../../../constants.dart';
 import 'rounded_icon_btn.dart';
 
-class CartCounter extends StatelessWidget {
-  const CartCounter({
-    Key? key,
-  }) : super(key: key);
+class CartCounter extends StatefulWidget {
+  
+  @override
+  _CartCounterState createState() => _CartCounterState();
+}
+
+class _CartCounterState extends State<CartCounter> {
+  int _counter = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -19,19 +23,27 @@ class CartCounter extends StatelessWidget {
         children: [
           RoundIconBtn(
             iconData: Icons.remove,
-            color: Colors.black38,
-            press: () {},
+            // color: Colors.black38,
+            press: () {
+              setState(() {
+                _counter -= 1;
+              });
+            },
           ),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: defaultPadding / 4),
             child: Text(
-              "1",
+              "$_counter",
               style: TextStyle(fontSize: 14, fontWeight: FontWeight.w800),
             ),
           ),
           RoundIconBtn(
             iconData: Icons.add,
-            press: () {},
+            press: () {
+              setState(() {
+                _counter += 1;
+              });
+            },
           ),
         ],
       ),
